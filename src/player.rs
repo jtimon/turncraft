6a59688bfc13a3936a88550d37aa94fea93dc1ed
Jorge Timón <jtimon@jtimon.cc>
2021-05-26 02:55:14 +0200

    Soldiers can now be used to attack and destroy the enemy

--- src/player.rs
+++ src/player.rs
@@ -2,11 +2,17 @@
 
 use crate::ui;
 
+struct Attack {
+    attacking_player: usize,
+    attacked_player: usize,
+    attacking_soldiers: u32,
+}
+
 struct Player {
     player_name: String,
-    money: u64,
-    population: u64,
-    soldiers: u64,
+    money: u32,
+    population: u32,
+    soldiers: u32,
     option: u32,
 }
 
@@ -18,11 +24,12 @@ impl Player {
 
 pub struct PlayerMan {
     players: Vec<Player>,
+    pending_attacks: Vec<Attack>,
 }
 
 impl PlayerMan {
     pub fn new() -> PlayerMan {
-        PlayerMan{players: Vec::<Player>::new()}
+        PlayerMan{players: Vec::<Player>::new(), pending_attacks: Vec::<Attack>::new()}
     }
 
     pub fn new_player(&mut self) {
@@ -54,17 +61,52 @@ impl PlayerMan {
     }
 
     pub fn select_turn(&mut self) {
-        for p in &mut self.players {
-            if p.option != 0 {
-                println!("Player: {}", p.player_name);
-                if p.money > 1 {
+        let players_len = self.players.len();
+        for i in 0..players_len {
+            if self.players[i].option != 0 {
+                println!("Player: {}", self.players[i].player_name);
+                if self.players[i].money > 0 && self.players[i].soldiers > 0 {
+                    println!("Options: 1) Trade 2) Housing 3) Soldiers 4) Attack 0) Surrender");
+                    self.players[i].option = ui::input_u32(0, 4);
+                } else if self.players[i].money > 0 {
                     println!("Options: 1) Trade 2) Housing 3) Soldiers 0) Surrender");
-                    p.option = ui::input_u32(0, 3);
+                    self.players[i].option = ui::input_u32(0, 3);
                 } else {
                     println!("Options: 1) Trade 2) Housing 0) Surrender");
-                    p.option = ui::input_u32(0, 2);
+                    self.players[i].option = ui::input_u32(0, 2);
+                }
+
+                if self.players[i].option == 4 {
+                    let mut attacked_player;
+                    // If there's only two players, just attack the other player
+                    if players_len == 2 {
+                        if i == 0 {
+                            attacked_player = 1;
+                        } else {
+                            attacked_player = 0;
+                        }
+                    } else {
+                        println!("Which player do you wish to attack?");
+                        for j in 0..players_len {
+                            if !self.players[j].player_name.eq(&self.players[i].player_name) {
+                                println!("{} {}", j, self.players[j].player_name);
+                            }
+                        }
+
+                        attacked_player = ui::input_u32(0, self.players.len() as u32) as usize;
+                        while attacked_player == i {
+                            println!("You can't attack yourself");
+                            attacked_player = ui::input_u32(0, self.players.len() as u32) as usize;
+                        }
+                    }
+
+                    println!("How many soldiers do you want to send for the attack ({} available)?", self.players[i].soldiers);
+                    let attacking_soldiers = ui::input_u32(1, self.players[i].soldiers);
+                    self.players[i].soldiers -= attacking_soldiers;
+                    self.pending_attacks.push(Attack{attacking_player: i, attacked_player, attacking_soldiers});
                 }
             }
+
         }
     }
 
@@ -77,6 +119,31 @@ impl PlayerMan {
             } else if p.option == 3 {
                 p.money -= 1;
                 p.soldiers += 1;
+            } else if p.option == 4 {
+                p.money -= 1;
+            }
+        }
+
+        for attack in &self.pending_attacks {
+            if self.players[attack.attacked_player].soldiers == 0 {
+                if attack.attacking_soldiers > self.players[attack.attacked_player].population {
+                    self.players[attack.attacked_player].population = 0;
+                } else {
+                    self.players[attack.attacked_player].population -= attack.attacking_soldiers;
+                }
+                self.players[attack.attacking_player].soldiers += attack.attacking_soldiers;
+            } else if attack.attacking_soldiers > self.players[attack.attacked_player].soldiers {
+                self.players[attack.attacked_player].soldiers = 0;
+                self.players[attack.attacking_player].soldiers += attack.attacking_soldiers - 1;
+            } else {
+                self.players[attack.attacked_player].soldiers -= 1;
+            }
+        }
+        self.pending_attacks.clear();
+
+        for p in &mut self.players {
+            if p.population == 0 {
+                p.option = 0;
             }
         }
     }
